0f549976c0d42f4fc2467887abb8885b4cc24232
Huda <hudashaik36@gamil.com>
2026-09-25 21:06:11 +0530

    Day 2 done

---
+++ day2.py
@@ -0,0 +1,18 @@
+# Day 2 - Data Types and Sum of two numbers
+
+# Data types example
+a = 10         # int
+b = 4.5        # float
+name = "Huda"     # str
+is_done = True    #boolean
+print("a is", a, "type is int")
+print("b is", b, "type is float")
+print("name is", name)
+print("is_done is", is_done)
+
+# Sum of two numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+total = num1 + num2
+print("Sum is:", total)
